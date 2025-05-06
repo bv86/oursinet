eaf07a008cec4aa2a57837670469a5d7f947ac88
Benoit VANNESSON <benoit.vannesson@gmail.com>
2025-05-06 23:36:42 +0200

    fix: better spacing between blog items

--- next/src/components/ContentList.tsx
+++ next/src/components/ContentList.tsx
@@ -32,7 +32,7 @@ export async function ContentList({
   const Component = component;
 
   return (
-    <section className="flex flex-col gap-8 items-center px-4">
+    <section className="flex flex-col gap-8 items-center">
       {showSearch && <Search />}
       <div className="w-full flex flex-col gap-4">
         {articles.map((article) => (
